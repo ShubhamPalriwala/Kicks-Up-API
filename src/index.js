92fabd5ae56a389ac9397690538c69d152cbf815
LaalShaitaan <spalriwalau@gmail.com>
2020-05-31 22:23:12 +0530

    Recent Updates

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const userRouter=require('./routers/userRouter')
 const productRouter=require('./routers/productRouter')
 
 const app=express()
-const port=process.env.PORT
+const port=process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/users',userRouter)
